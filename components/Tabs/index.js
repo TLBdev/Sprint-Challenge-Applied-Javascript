eaa8d886489d47f95d2ab49cd2aca6ce2161068a
tony <serratedhumor@yahoo.com>
2019-11-01 10:29:36 -0500

    Tabs comblete

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,17 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+let topics = document.querySelector('.topics')
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(response => {
+        response.data.topics.forEach(element => {
+            topics.appendChild(Tabs(element))
+        });
+    })
+
+function Tabs(input){
+    let tab = document.createElement('div')
+    tab.classList.add('tab')
+    tab.textContent = input
+    return tab
+}
