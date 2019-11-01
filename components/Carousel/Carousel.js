227ec42ca8b30d1acd49748877725db00dce652e
tony <serratedhumor@yahoo.com>
2019-11-01 11:48:50 -0500

    carousel complete

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,80 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function carouselCreator(){
+  //create
+  const carousel = document.createElement('div'),
+  leftButton = document.createElement('div'),
+  img1 = document.createElement('img'),
+  img2 = document.createElement('img'),
+  img3 = document.createElement('img'),
+  img4 = document.createElement('img'),
+  rightButton = document.createElement('div')
+  //nest
+  carousel.append(leftButton, img1, img2, img3, img4, rightButton)
+  //classes
+  carousel.classList.add('carousel')
+  leftButton.classList.add('left-button')
+  rightButton.classList.add('right-button')
+  //content
+  img1.src = "./assets/carousel/mountains.jpeg"
+  img2.src = "./assets/carousel/computer.jpeg" 
+  img3.src = "./assets/carousel/trees.jpeg"
+  img4.src = "./assets/carousel/turntable.jpeg"
+  //functionality
+  let arr = [img1, img2, img3, img4]
+  let counter = 0
+  if(counter === 0){
+    img1.style.display = 'inline-block'
+  }
+  leftButton.addEventListener('click', () => {
+    if (counter === 2){
+      counter --
+      img1.style.display = 'none'
+      img2.style.display = 'inline-block'
+      img3.style.display = 'none'
+      img4.style.display = 'none'
+    }else if (counter === 3){
+      counter --
+      img1.style.display = 'none'
+      img2.style.display = 'none'
+      img3.style.display = 'inline-block'
+      img4.style.display = 'none'
+    }else if (counter === 1){
+      counter --
+      img1.style.display = 'inline-block'
+      img2.style.display = 'none'
+      img3.style.display = 'none'
+      img4.style.display = 'none'
+    }
+      
+  })
+  rightButton.addEventListener('click', () => {
+    if (counter === 0){
+      counter ++
+      img1.style.display = 'none'
+      img2.style.display = 'inline-block'
+      img3.style.display = 'none'
+      img4.style.display = 'none'
+    }else if (counter === 1){
+      counter ++
+      img1.style.display = 'none'
+      img2.style.display = 'none'
+      img3.style.display = 'inline-block'
+      img4.style.display = 'none'
+    }else if (counter === 2){
+      counter ++
+      img1.style.display = 'none'
+      img2.style.display = 'none'
+      img3.style.display = 'none'
+      img4.style.display = 'inline-block'
+    }
+  })
+  
+  return carousel
+}
+
+let carouselCont = document.querySelector('.carousel-container')
+
+carouselCont.appendChild(carouselCreator())
